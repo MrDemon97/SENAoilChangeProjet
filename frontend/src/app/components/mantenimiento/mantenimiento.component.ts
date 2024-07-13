3f3c4dcf4a18ea4dace0f8473a776165cf18e30d
Jair Coral <jaircoral88@gmail.com>
2024-07-12 22:21:33 -0500

    "Added imports, service, and model to MantenimientoComponent; implemented OnInit and initialized mantenimientos array"

--- frontend/src/app/components/mantenimiento/mantenimiento.component.ts
+++ frontend/src/app/components/mantenimiento/mantenimiento.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MantenimientoService } from '../../services/mantenimiento.service';
+import { Mantenimiento } from '../../models/mantenimiento.model';
 
 @Component({
   selector: 'app-mantenimiento',
-  standalone: true,
-  imports: [],
   templateUrl: './mantenimiento.component.html',
-  styleUrl: './mantenimiento.component.css'
+  styleUrls: ['./mantenimiento.component.css']
 })
-export class MantenimientoComponent {
+export class MantenimientoComponent implements OnInit {
+  mantenimientos: Mantenimiento[] = [];
 
+  constructor(private mantenimientoService: MantenimientoService) {}
+
+  ngOnInit() {
+    // Obtener todos los mantenimientos
+    this.mantenimientoService.getMantenimientos().subscribe(data => {
+      this.mantenimientos = data;
+    });
+
+    // Obtener mantenimientos filtrados por nombre del técnico
+    this.mantenimientoService.getMantenimientos({ tecnicoNombre: 'Juan Perez' }).subscribe(data => {
+      console.log('Mantenimientos por técnico:', data);
+    });
+  }
 }
