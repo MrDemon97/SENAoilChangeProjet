2dbe7d727c6f6676cb436dc9f8473a360721d7e1
Jair Coral <jaircoral88@gmail.com>
2024-07-13 19:44:19 -0500

    Corriegiendo erroes

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
@@ -9,7 +8,7 @@ import { AppRoutingModule } from './app-routing.module';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [BrowserModule, FormsModule, HttpClientModule, AppRoutingModule, RouterOutlet],
+  imports: [FormsModule, HttpClientModule, AppRoutingModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
